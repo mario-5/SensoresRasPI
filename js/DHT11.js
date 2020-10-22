383f7a15a44c32d05cf9b6042766609ff946c405
mario-5 <mario@iotarch.com.ar>
2020-10-22 00:08:59 -0300

    comentarios

--- js/DHT11.js
+++ js/DHT11.js
@@ -26,6 +26,7 @@ sensor.read(11, 23, function(err, temperature, humidity) {
   ws.POSTcriaserver ("K1",dia,hora,"H",temp,"T");
   ws.POSTcriaserver ("K1",dia,hora,"H",humi,"H");
   utiles.logfs (temp+" "+humi+" "+hora);
+  //agrego mensaje via Telegram bot RaspMsg
   ws.POSTTelegram ("K1",dia,hora,"H",temp,"T");
 });
 
