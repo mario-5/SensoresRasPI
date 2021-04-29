bd8121bc046f94f6958d67385393aa9611071f05
mario-5 <mario@iotarch.com.ar>
2021-04-29 09:54:18 -0300

    ajustes de lectura

--- js/agua.js
+++ js/agua.js
@@ -1,7 +1,8 @@
 const Gpio = require('onoff').Gpio;
 //const led = new Gpio(17, 'out');
-const agua = new Gpio(21, 'in', 'both');
-
+const agua = new Gpio(21, 'in', 'both',{debunceTimeout:50});
+actual = agua.readSync();
+console.log("lectura actual "+actual);
 agua.watch((err, value) => {
   if (err) {
     throw err;
@@ -12,7 +13,7 @@ agua.watch((err, value) => {
   console.log ("Valor 1 sin 0 con agua "+value);
 });
 
-//process.on('SIGINT', _ => {
+process.on('SIGINT', _ => {
   //led.unexport();
- // agua.unexport();
-//});
+ agua.unexport();
+});
